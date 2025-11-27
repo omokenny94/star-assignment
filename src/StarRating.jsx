4380953ebd71840c864bcbadb0a85cf97ae0c222
omokenny94 <omokenny94@yahoo.com>
2025-11-27 16:57:26 +0100

    Updated

--- src/StarRating.jsx
+++ src/StarRating.jsx
@@ -1,29 +1,46 @@
+import {range} from "./util.js"
+
+// function StarRating({ rating }) {
+//   /*
+//     Here's the markup for a single star:
+
+//     <img
+//       alt=""
+//       className="gold-star"
+//       // src="https://sandpack-bundler.vercel.app/img/gold-star.svg"
+//       src="/star.svg"
+//     />
+
+//     Your job is to repeat this element
+//     based on the `rating` prop.
+//     If the rating is 4, we need 4 copies.
+//   */
+
+//   return (
+//     <div className="star-wrapper">
+//       <img
+//         key={crypto.randomUUID()}
+//         alt=""
+//         className="gold-star"
+//         src="/star.svg"
+//       />
+//     </div>
+//   )
+// }
+
 function StarRating({ rating }) {
-  /*
-    Here's the markup for a single star:
-
-    <img
-      alt=""
-      className="gold-star"
-      // src="https://sandpack-bundler.vercel.app/img/gold-star.svg"
-      src="/star.svg"
-    />
-
-    Your job is to repeat this element
-    based on the `rating` prop.
-    If the rating is 4, we need 4 copies.
-  */
-
   return (
     <div className="star-wrapper">
-      <img
-        key={crypto.randomUUID()}
-        alt=""
-        className="gold-star"
-        src="/star.svg"
-      />
+      {range(rating).map((n) => (
+        <img
+          key={n}
+          alt=""
+          className="gold-star"
+          src="/star.svg"
+        />
+      ))}
     </div>
-  )
+  );
 }
 
 export default StarRating
