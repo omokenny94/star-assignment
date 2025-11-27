aae233123c8da3238aa3946e5181466c89ed27fc
null <null>
2025-11-27 16:46:57 +0100

    Initial Commit

--- src/util.js
+++ src/util.js
@@ -3,6 +3,14 @@ export function range(start, end, step = 1) {
   const output = []
 
   //  implement a range function
+  if (end === undefined) {
+    end = start;
+    start = 0;
+  }
+
+  for (let i = start; i < end; i += step) {
+    output.push(i);
+  }
 
   return output
 }
@@ -13,3 +21,6 @@ range(2, 10) // start-2, end-10
 
 range(0, 10, 2) // start- , end-10, step- 2 [0, 2, 4, 6, 8]
 range(5) // [0, 1, 2, 3, 4]
+
+
+export default range
